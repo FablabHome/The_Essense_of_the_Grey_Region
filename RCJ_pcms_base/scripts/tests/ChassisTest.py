8893c34cd1eae3ad313ca4c9641458cd42e77218
rootadminwalker <chiioleong519@gmail.com>
2021-09-13 17:29:41 +0800

    Test turn_to function

--- RCJ_pcms_base/scripts/tests/ChassisTest.py
+++ RCJ_pcms_base/scripts/tests/ChassisTest.py
@@ -1,13 +1,8 @@
-from time import sleep
-
 import rospy
-from geometry_msgs.msg import Point
+from core.tools import Chassis
 
 rospy.init_node('test_chassis')
-
-pub = rospy.Publisher("/client/chassis_slam/point", Point, queue_size=30)
-
-goal_point = Point(x=-0.65588299604, y=8.25853904999, z=0.994025686939)
-original_point = Point(x=2.08599934321, y=5.11935406091, z=0.703878971997)
-
-pub.publish(goal_point)
+chassis = Chassis()
+rospy.sleep(1)
+# chassis.turn(3.14)
+chassis.check_yaw()
