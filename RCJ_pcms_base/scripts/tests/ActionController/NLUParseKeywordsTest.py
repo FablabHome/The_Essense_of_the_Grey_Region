18d47cd7455bdb641be1e9f3595e2964e2df8f5a
rootadminwalker <chiioleong519@gmail.com>
2021-09-20 19:03:29 +0800

    Saved the NLU model

--- RCJ_pcms_base/scripts/tests/ActionController/NLUParseKeywordsTest.py
+++ RCJ_pcms_base/scripts/tests/ActionController/NLUParseKeywordsTest.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-import json
 
 from snips_nlu import SnipsNLUEngine
-from snips_nlu.default_configs import CONFIG_EN
 
-with open('./dataset.json') as f:
-    sample_dataset = json.load(f)
-
-nlu_engine = SnipsNLUEngine(config=CONFIG_EN)
-nlu_engine = nlu_engine.fit(sample_dataset)
+nlu_engine = SnipsNLUEngine.from_path('./test_engine')
 try:
     while True:
         text = input('Your command: \n|__ ')
