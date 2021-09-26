2c28a70752e05602041c50f5fe4775fbc62e850d
rootadminwalker <chiioleong519@gmail.com>
2021-09-26 23:55:56 +0800

    :star: new(snips parser): New dataset 1, Reformatted the directory structure of the datasets 2, Created a new dataset to order food in restaurant for testing 3, :boom Currently discovered a way to deal with take out or to not in ordering food situation

--- RCJ_pcms_base/scripts/tests/ActionController/NLUParseKeywordsTest.py
+++ RCJ_pcms_base/scripts/tests/ActionController/NLUParseKeywordsTest.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+import json
 
 from snips_nlu import SnipsNLUEngine
+from snips_nlu.default_configs import CONFIG_EN
 
-nlu_engine = SnipsNLUEngine.from_path('./test_engine')
+with open('datasets/orderfood/dataset.json') as f:
+    sample_dataset = json.load(f)
+
+nlu_engine = SnipsNLUEngine(config=CONFIG_EN)
+nlu_engine = nlu_engine.fit(sample_dataset)
 try:
     while True:
         text = input('Your command: \n|__ ')
