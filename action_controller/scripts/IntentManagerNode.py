c306366ab16426696000666b5bbd4095fd415ce0
rootadminwalker <chiioleong519@gmail.com>
2022-09-02 17:08:14 +0800

    Voice Assistant Revamp

--- action_controller/scripts/IntentManagerNode.py
+++ action_controller/scripts/IntentManagerNode.py
@@ -25,22 +25,24 @@ SOFTWARE.
 """
 import hashlib
 import json
+import os
 from copy import copy
 from datetime import datetime
 
 import actionlib
 import rospy
 from home_robot_msgs.msg import IntentManagerAction, IntentManagerGoal, Blacklist, IntentACControllerAction, \
-    IntentACControllerGoal, IntentManagerResult, IntentManagerFeedback
-from home_robot_msgs.srv import Session, SessionRequest, SessionResponse
+    IntentACControllerGoal, IntentManagerResult, VoiceSession
+from home_robot_msgs.srv import Session, SessionRequest, SessionResponse, ContinueSess, ContinueSessResponse, \
+    ContinueSessRequest
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
 from rich.traceback import install
 from std_srvs.srv import Trigger, TriggerResponse
 
-from core.Dtypes import IntentConfigs
-from core.Nodes import Controller
+from core.Dtypes import IntentConfigs, Slots
+from core.Nodes import Node
 from core.utils import HeySnipsNLUParser
 
 console = Console()
@@ -48,16 +50,13 @@ print = console.print
 install(show_locals=True)
 
 
-class IntentManager(Controller):
+class IntentManager(Node):
     def __init__(self):
         super(IntentManager, self).__init__(
             name="intent_manager",
             anonymous=False,
-            default_state='NORMAL',
-            state_param_group='/intent/',
-            states=['NORMAL', 'ON_SESSION', 'SLOT_MISSING']
         )
-        with console.status("[yellow] Starting the intent_manager") as status:
+        with console.status("[yellow] Starting the intent_manager") as _:
             # Load the intent configs
             console.log("Loading configs")
             config_file = rospy.get_param('~config_file')
@@ -65,11 +64,17 @@ class IntentManager(Controller):
 
             # Load the nlu engines
             console.log("[cyan]Loading NLU engine")
-            engine_path = rospy.get_param('~engine_path')
-            engine_name = rospy.get_param('~engine_name')
-            self.nlu_engine = HeySnipsNLUParser(engine_configs={
-                engine_name: engine_path
-            })
+            engine_configs = {}
+            dataset_configs = {}
+            for engine_path in self.intent_configs.engines:
+                engine_name = engine_path.split(os.path.sep)[-1]
+                engine_configs[engine_name] = engine_path
+
+            for dataset_path in self.intent_configs.datasets:
+                dataset_name = dataset_path.split(os.path.sep)[-1].split('.')[0]
+                dataset_configs[dataset_name] = dataset_path
+
+            self.nlu_engine = HeySnipsNLUParser(engine_configs=engine_configs)
 
             # The Intent blacklist from boss
             self.intent_blacklist = []
@@ -83,16 +88,12 @@ class IntentManager(Controller):
                 queue_size=1
             )
 
-            # Create the session request entry
-            console.log("Initializing services")
             rospy.Service('~start_session', Session, self.start_session_cb)
-            # Create the continue session entry
-            rospy.Service('~continue_session', Session, self.continue_session_cb)
-            # Create the stop session entry
+            rospy.Service('~continue_session', ContinueSess, self.continue_session_cb)
             rospy.Service('~stop_session', Trigger, self.stop_session_cb)
 
-            # Create a instance to call the ActionController
-            self.action_controller = actionlib.SimpleActionClient(
+            # Create an instance to call the ActionController
+            self.action_evaluator = actionlib.SimpleActionClient(
                 'intent_ac',
                 IntentACControllerAction
             )
@@ -108,56 +109,66 @@ class IntentManager(Controller):
             self.manager_server.start()
 
             # session variables
-            self.session = None
-            rospy.set_param('~on_session', False)
+            self.session = VoiceSession()
 
         print("[bold green]intent_manager is online")
         self.main()
 
-    """
-    ********************************* Session callbacks starts here ***********************************
-    """
-    def start_session_cb(self, req: SessionRequest):
-        if not self.__on_session():
-            # Generate session ID
-            req.session_data.id = self.__generate_session_id()
-            console.log(f'[bold yellow]A session has been established with the following parameters:')
-            self.__show_session_summary(req.session_data)
-            self.__establish_session(req.session_data)
+    def blacklist_cb(self, blacklist):
+        pass
+
+    def get_current_resp(self, current_intent, missing_slots):
+        confirm_responses = self.intent_configs.intents[current_intent].confirm_responses
+        missing_slots = set(missing_slots)
+        for resp_key, confirm_resp in confirm_responses.__dict__.items():
+            if set(resp_key.split()) == missing_slots:
+                return confirm_resp
+
+    def start_session_cb(self, session: SessionRequest):
+        self.session = copy(session.session_data)
+        self.session.id = self.__generate_session_id()
+        self.session.session_type = 'NORMAL'
+        self.session.max_rounds = 1
+        self.session.elapsed_rounds = 0
         return SessionResponse()
 
-    def continue_session_cb(self, req: SessionRequest):
-        if self.__on_session():
-            console.log(f'[bold yellow]The session has been continued with the following parameters:')
-            self.__show_session_summary(req.session_data)
-            self.__establish_session(req.session_data)
-        return SessionResponse()
+    def continue_session_cb(self, continue_req: ContinueSessRequest):
+        self.session.custom_data = continue_req.custom_data
+        self.session.possible_next_intents = continue_req.possible_next_intents
+        self.session.max_rounds += 1
+        return ContinueSessResponse()
+
+    def start_confirm_session(self, current_intent, slots, max_re_ask, missing_slots):
+        assert not self.on_session(), f"A session with\nID: {self.session.id}\ntype:{self.session.session_type}\nis on"
+
+        pass_data = {'confirm_resp': self.get_current_resp(current_intent, missing_slots), 'slots': slots.raw_slots}
+        self.session = VoiceSession()
+        self.session.id = self.__generate_session_id()
+        self.session.started_intent = current_intent
+        self.session.possible_next_intents = [current_intent]
+        if (ci := self.intent_configs.intents[current_intent].confirm_intent) != '':
+            self.session.possible_next_intents.append(ci)
+        self.session.custom_data = json.dumps(pass_data)
+        self.session.session_type = 'CONFIRM'
+        self.session.max_rounds = max_re_ask
+        self.session.elapsed_rounds = 0
+
+    def update_confirm_session(self, this_time_slots, missing_slots):
+        assert self.session.session_type == 'CONFIRM', "Current session running is not a confirm session"
+        pass_data = json.loads(self.session.custom_data)
+        saved_slots = pass_data['slots']
+        saved_slots = Slots(saved_slots)
+        saved_slots.update_but_ignore_exist_slots(this_time_slots.raw_slots)
+        pass_data['confirm_resp'] = self.get_current_resp(self.session.started_intent, missing_slots)
+        pass_data['slots'] = saved_slots.raw_slots
+        self.session.custom_data = json.dumps(pass_data)
 
     def stop_session_cb(self, req):
-        if self.__on_session():
-            rospy.set_param('~on_session', False)
-            self.session = None
+        self.session = VoiceSession()
         return TriggerResponse()
-    """
-    ********************************* Session callbacks ends here *************************************
-    """
 
-    def blacklist_cb(self, blacklist: Blacklist):
-        self.intent_blacklist = blacklist.blacklist
-
-    """
-    ///////////////////////////////// Helper functions starts here /////////////////////////////////////
-    """
-    @staticmethod
-    def __on_session():
-        return rospy.get_param('~on_session')
-
-    def __intent_in_next_intents(self, intent):
-        return (intent in self.session.possible_next_intents) or len(self.session.possible_next_intents) == 0
-
-    def __establish_session(self, session):
-        rospy.set_param('~on_session', True)
-        self.session = copy(session)
+    def on_session(self):
+        return self.session.id != ''
 
     @staticmethod
     def __show_session_summary(session):
@@ -166,7 +177,10 @@ class IntentManager(Controller):
         summary.add_column('Value')
         summary.add_row('session_id', session.id)
         summary.add_row('started_intent', session.started_intent)
+        summary.add_row('session_type', session.session_type)
         summary.add_row('custom_data', session.custom_data)
+        summary.add_row('flowed_intents', '-->'.join(session.flowed_intents))
+        summary.add_row('possible_next_intents', ', '.join(session.possible_next_intents))
         summary.add_row('max_rounds', Text(str(session.max_rounds), style='bold magenta'))
         summary.add_row('elapsed_rounds', str(session.elapsed_rounds))
         print(summary)
@@ -195,97 +209,59 @@ class IntentManager(Controller):
         sess_id = hashlib.sha256(date.encode())
         return sess_id.hexdigest()
 
-    def __handle_feedback(self):
-        feedback = IntentManagerFeedback()
-        feedback.status = 'Accepted'
-        self.manager_server.publish_feedback(feedback)
-
-    def __check_if_end_session(self):
-        # If no session is going to establish, end the session
-        if self.__on_session():
-            if self.session.elapsed_rounds >= self.session.max_rounds:
-                self.stop_session_cb(None)
-
-    """
-    ///////////////////////////////// Helper functions ends here /////////////////////////////////////
-    """
-
-    def voice_cb(self, voice_goal: IntentManagerGoal):
-        # Set the state to NORMAL first, if there isn't any request, it will stay the same
-        # self.set_state('NORMAL')
-
-        # Get the text from the goal
-        raw_text = voice_goal.text
-        console.log(f'[bold]Parsing text: "{raw_text}"')
-
-        # Feedback to the voice server that it was successes
-        self.__handle_feedback()
-
-        # Parse the intent from snips-nlu
-        result = self.nlu_engine.parse(raw_text)
-        intent = result.intent
-        slots = result.slots
-
-        # Show the report
-        console.log(f"Text '{raw_text}' successfully parsed, parsing result:")
-        self.__show_nlu_report(intent, slots)
-
+    def parse_data(self, text):
+        _, intent, _, slots, = self.nlu_engine.parse(text)[-1]
+        slots = Slots(slots)
         if intent is None:
-            console.log(f"[bold red]Text '{raw_text}' doesn't match any intents")
             intent = 'NotRecognized'
 
-        # Ignore the intent if it was in the blacklist
-        if intent in self.intent_blacklist:
-            console.log(f'[bold red]Intent {intent} was currently blacklisted by boss, ignoring')
-            return
+        missing_slots = self.intent_configs.find_missing_slots(intent, slots)
+        if self.on_session():
+            if self.session.session_type == 'NORMAL':
+                if intent not in self.session.possible_next_intents:
+                    intent = 'NotRecognized'
+            elif self.session.session_type == 'CONFIRM':
+                missing_slots = set(missing_slots) - set(
+                    Slots(json.loads(self.session.custom_data)['slots']).list_slot_names())
+                if len(missing_slots) > 0:
+                    self.update_confirm_session(slots, missing_slots)
+                    intent = 'Confirm'
+                else:
+                    self.stop_session_cb(None)
+        else:
+            if len(missing_slots) > 0:
+                if self.session.session_type != 'CONFIRM':
+                    self.start_confirm_session(intent, slots, self.intent_configs.intents[intent].max_re_ask,
+                                               missing_slots)
+                intent = 'Confirm'
 
-        # Get intent configs from the config file
-        try:
-            intent_config = self.intent_configs[intent]
-        except KeyError:
-            console.log(f"[yellow]Intent {intent}'s config doesn't exist, using default")
-            intent_config = IntentConfigs.INTENT_DEFAULT_CONFIG
+        return intent, slots
 
-        # Send the intent to the snips_intent_ac
-        console.log('Sending intent goal to /intent_ac')
+    def voice_cb(self, voice_goal: IntentManagerGoal):
+        text = voice_goal.text
+        send_intent, slots = self.parse_data(text)
 
-        # Record the intent if it was on session
-        if self.__on_session():
-            self.session.elapsed_rounds += 1
-            if self.__intent_in_next_intents(intent):
-                self.session.flowed_intents.append(intent)
-            else:
+        if self.on_session():
+            if self.session.elapsed_rounds >= self.session.max_rounds:
+                if self.session.session_type == 'CONFIRM':
+                    send_intent = 'NotRecognized'
                 self.stop_session_cb(None)
-                intent = "NotRecognized"
+            else:
+                console.log("This is a session running currently:")
+                self.__show_session_summary(self.session)
+                self.session.elapsed_rounds += 1
 
-        # Serialize information into ROS message
-        intent_goal = IntentACControllerGoal()
-        intent_goal.intent = intent
-        intent_goal.slots = json.dumps(slots.raw_slots)
-        intent_goal.raw_text = raw_text
-        if self.__on_session():
-            intent_goal.session = self.session
+        self.__show_nlu_report(send_intent, slots)
 
-        self.action_controller.send_goal(intent_goal)
-        console.log('[bold green]Intent goal sent')
+        goal = IntentACControllerGoal()
+        goal.intent = send_intent
+        goal.slots = json.dumps(slots.raw_slots)
+        goal.raw_text = text
+        goal.session = self.session
 
-        # Show preempt info
-        allow_preempt = intent_config.allow_preempt
-        allow_msg = 'allowed' if allow_preempt else 'not allowed'
-        console.log(f"[cyan]Intent {intent} was {allow_msg} to be preempted")
-
-        # If the intent wasn't allowed to be preempted, wait until there's a result
-        # else, don't wait
-        if not allow_preempt:
-            with console.status('[yellow]Waiting for the intent to be executed') as status:
-                self.action_controller.wait_for_result()
-            print('[bold green]Intent executed successfully')
-
-        result = IntentManagerResult(True)
-        self.manager_server.set_succeeded(result, 'The intent was successfully handled')
-
-        # If no session is going to establish, end the session
-        self.__check_if_end_session()
+        self.action_evaluator.send_goal(goal)
+        self.action_evaluator.wait_for_result()
+        self.manager_server.set_succeeded(IntentManagerResult(self.on_session()))
 
     def main(self):
         while not rospy.is_shutdown():
---
+++ config/ActionController/SnipsIntentConfigs/beverage.hcl
@@ -0,0 +1,14 @@
+engines = [
+    "/home/rootadminwalker/workspace/ROS_projects/src/The_Essense_of_the_Grey_Region/action_controller/engines/beverage"
+]
+
+intent prepareBeverage {
+    required_slots = ["beverage_type", "number_of_cups"]
+    max_re_ask = 3
+    confirm_intent = ""
+    confirm_responses = {
+        "beverage_type" = "Can you tell me what do you want to order?"
+        "number_of_cups" = "Can you tell me how much you'd like to order?"
+        "number_of_cups beverage_type" = "Can you tell me what do you want to order and how much?"
+    }
+}
